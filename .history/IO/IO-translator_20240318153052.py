84c30c0194b55929dc98441e486b30f3ac785b8c
Gopal Gurram <gopalgurram1@gmail.com>
2024-03-18 15:31:24 +0530

    add lang translator

---
+++ .history/IO/IO-translator_20240318153052.py
@@ -0,0 +1,13 @@
+from translate import Translator
+
+translator = Translator(to_lang='ja')
+
+try:
+    with open('text.txt', mode='r') as my_file:
+        text = my_file.read()
+        translation = translator.translate(text)
+        with open('text-ja.txt', mode='w') as my_file2:
+            my_file2.write(translation)
+except FileNotFoundError as e:
+    print('Check your file path')
+raise e
